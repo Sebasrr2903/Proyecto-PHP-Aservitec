21cb1469a427c25a132b50aedaeab57bb7c97d21
Ronny <ronny199528@gmail.com>
2023-03-12 13:40:20 -0600

    Pruebas Conexión

--- Model/conexion.php
+++ Model/conexion.php
@@ -5,20 +5,24 @@ $conn = "";
 try {
 	$servername = "localhost";
 	$dbname = "aservitecdb";
-	$username = "root";
-	$password = "Palomo19952813.";
+	$username = "admin";
+	$password = "123456";
 
 	$conn = new PDO(
-		"mysql:host=$servername; dbname=geeksforgeeks",
+		"mysql:host=$servername; dbname=aservitecdb",
 		$username, $password
 	);
 	
-$conn->setAttribute(PDO::ATTR_ERRMODE,
-					PDO::ERRMODE_EXCEPTION);
+
 }
 catch(PDOException $e) {
 	echo "Connection failed: " . $e->getMessage();
 }
 
+public_function getConexion(){
+	$conn->setAttribute(PDO::ATTR_ERRMODE,
+	PDO::ERRMODE_EXCEPTION);
+	return $conn;
+}
 ?>
 
